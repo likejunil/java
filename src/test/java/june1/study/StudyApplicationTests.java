88b9542b619c2b5cb08273b9975675d93fb35e1e
권준일 <likejunil@gmail.com>
2021-12-13 21:22:07 +0900

    chore: 기본 폴더 및 파일을 셋팅하라

---
+++ src/test/java/june1/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package june1.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

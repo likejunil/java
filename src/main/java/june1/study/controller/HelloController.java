d7377d63df20741965d794aaf8d69a0e269ec6fa
권준일 <likejunil@gmail.com>
2021-12-13 21:53:16 +0900

    feat: hello 컨트롤러를 추가하라

---
+++ src/main/java/june1/study/controller/HelloController.java
@@ -0,0 +1,17 @@
+package june1.study.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "ok";
+    }
+}
